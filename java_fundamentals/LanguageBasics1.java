84c6e5c20390e67045dbc64f7f7733df0e262e9a
Kavyamunjala <munjalakavya15@gmail.com>
2025-07-16 22:01:18 +0530

    Add files via upload

---
+++ java_fundamentals/LanguageBasics1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class LanguageBasics1{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter the string:");
+        String a=scan.next();
+        System.out.println("enter the string1:");
+        String b=scan.next();
+        System.out.println(a+ "technologies" +b);
+    }
+    
+}
